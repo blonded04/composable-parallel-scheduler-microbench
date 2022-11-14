8ae8d660a3e54b3b2745881117ff7d8a9445ff1b
lejabque <vorkdenis1@gmail.com>
2022-11-14 22:58:55 +0000

    update scheduling bench

--- benchmarks/bench_scheduling.cpp
+++ benchmarks/bench_scheduling.cpp
@@ -1,31 +1,59 @@
 #include "parallel_for.h"
 #include <benchmark/benchmark.h>
 #include <mutex>
+#include <ratio>
 
 static constexpr size_t MIN_SIZE = 1;
 static constexpr size_t MAX_SIZE = 1 << 10;
 
-static size_t totalGen = 1;
-static thread_local size_t reportedGen = 0;
+namespace {
+struct TimeReporter {
+  // saves time of first report in current generation
+  void ReportTime(std::chrono::system_clock::time_point before) {
+    if (reportedGen < totalGen) {
+      auto now = std::chrono::system_clock::now();
+      auto duration =
+          std::chrono::duration_cast<std::chrono::nanoseconds>(now - before);
+      std::lock_guard<std::mutex> lock(mutex);
+      times.push_back(duration);
+      reportedGen = totalGen;
+    }
+  }
+
+  static std::vector<std::chrono::nanoseconds> GetTimes() {
+    std::lock_guard<std::mutex> lock(mutex);
+    return times;
+  }
+
+  static void Reset() {
+    std::lock_guard<std::mutex> lock(mutex);
+    times.clear();
+    totalGen++;
+  }
+
+  static inline size_t totalGen = 1;
+
+private:
+  size_t reportedGen = 0;
+
+  // common for all threads
+  static inline std::mutex mutex;
+  static inline std::vector<std::chrono::nanoseconds> times; // nanoseconds
+};
+} // namespace
+
+static thread_local TimeReporter timeReporter;
 
 static void BM_Scheduling(benchmark::State &state) {
-  std::mutex m;
   for (auto _ : state) {
-    std::vector<double> times;
     auto start = std::chrono::high_resolution_clock::now();
-    ParallelFor(0, state.range(0), [&](size_t i) {
-      if (reportedGen < totalGen) {
-        auto scheduled = std::chrono::high_resolution_clock::now();
-        auto schedulingTime =
-            std::chrono::duration_cast<std::chrono::duration<double>>(
-                scheduled - start);
-        std::lock_guard<std::mutex> lock(m);
-        times.push_back(schedulingTime.count());
-        reportedGen = totalGen;
-      }
-    });
-    state.SetIterationTime(*std::min_element(times.begin(), times.end()));
-    totalGen++;
+    ParallelFor(0, state.range(0),
+                [&](size_t i) { timeReporter.ReportTime(start); });
+    TimeReporter::totalGen++;
+    auto times = TimeReporter::GetTimes();
+    std::sort(times.begin(), times.end());
+    state.SetIterationTime(times.front().count() * 1e-9);
+    TimeReporter::Reset();
   }
 }
 
