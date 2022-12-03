c791a8f1ee52e45330df96d59cca5d7b40b6ca6e
lejabque <vorkdenis1@gmail.com>
2022-12-03 16:35:39 +0000

    use rdtsc

--- benchmarks/bench_scheduling.cpp
+++ benchmarks/bench_scheduling.cpp
@@ -8,22 +8,33 @@
 #include <vector>
 
 namespace {
+
+static uint64_t Now() {
+#ifdef __x86_64__
+  return __rdtsc();
+#elif __arm__
+  uint64_t val;
+  asm volatile("mrs %0, cntvct_el0" : "=r"(val));
+  return val;
+#endif
+}
+
 struct TimeReporter {
   // saves time of first report in current generation
   // we need generations to reset times in all threads after each benchmark
   // just incrementing generation
-  void ReportTime(std::chrono::system_clock::time_point before) {
+  void ReportTime(uint64_t before) {
+    auto duration = Now() - before;
     if (reportedGen < totalGen) {
-      auto now = std::chrono::system_clock::now();
-      auto duration =
-          std::chrono::duration_cast<std::chrono::nanoseconds>(now - before);
+      // lock is aquired only once per generation
+      // so it doesn't affect result performance
       std::lock_guard<std::mutex> lock(mutex);
       times.push_back(duration);
       reportedGen = totalGen;
     }
   }
 
-  static std::vector<std::chrono::nanoseconds> GetTimes() {
+  static std::vector<uint64_t> GetTimes() {
     std::lock_guard<std::mutex> lock(mutex);
     return times;
   }
@@ -41,14 +52,14 @@ private:
 
   // common for all threads
   static inline std::mutex mutex;
-  static inline std::vector<std::chrono::nanoseconds> times; // nanoseconds
+  static inline std::vector<uint64_t> times; // nanoseconds
 };
 } // namespace
 
 static thread_local TimeReporter timeReporter;
 
-static std::vector<std::chrono::nanoseconds> runOnce(size_t threadNum) {
-  auto start = std::chrono::high_resolution_clock::now();
+static std::vector<uint64_t> runOnce(size_t threadNum) {
+  auto start = Now();
   // TODO: configure number of tasks to be sure that all threads are used?
   auto tasksNum = threadNum * 100;
   auto totalBenchTime = std::chrono::duration<double>(1);
@@ -64,6 +75,20 @@ static std::vector<std::chrono::nanoseconds> runOnce(size_t threadNum) {
   return times;
 }
 
+static void printTimes(size_t threadNum, const std::vector<uint64_t> &result) {
+  std::cout << "{";
+  std::cout << "\"thread_num\": " << threadNum << ", ";
+  std::cout << "\"used_threads\": " << result.size() << ", ";
+  std::cout << "\"start_times\": [";
+  for (size_t i = 0; i < result.size(); i++) {
+    std::cout << result[i];
+    if (i != result.size() - 1) {
+      std::cout << ", ";
+    }
+  }
+  std::cout << "]}" << std::endl;
+}
+
 int main(int argc, char **argv) {
   auto threadNum = GetNumThreads();
   if (argc > 1) {
@@ -72,23 +97,16 @@ int main(int argc, char **argv) {
   runOnce(threadNum); // just for warmup
 
   size_t repeat = 20;
-  std::vector<std::chrono::nanoseconds> result(threadNum);
+  std::vector<uint64_t> result(threadNum);
   for (size_t i = 0; i < repeat; i++) {
     auto times = runOnce(threadNum);
     for (size_t j = 0; j < threadNum; j++) {
       result[j] += times[j];
     }
   }
-  std::cout << "{";
-  std::cout << "\"thread_num\": " << threadNum << ", ";
-  std::cout << "\"used_threads\": " << result.size() << ", ";
-  std::cout << "\"start_times\": [";
-  for (size_t i = 0; i < result.size(); i++) {
-    std::cout << result[i].count() / repeat;
-    if (i != result.size() - 1) {
-      std::cout << ", ";
-    }
+  for (size_t j = 0; j < threadNum; j++) {
+    result[j] /= repeat;
   }
-  std::cout << "]}" << std::endl;
+  printTimes(threadNum, result);
   return 0;
 }
