44ee2725b48e63ace4d5bbcf2aefbab0c063e158
lejabque <vorkdenis1@gmail.com>
2023-02-02 21:19:28 +0000

    optionally enable rapid

--- benchmarks/bench_reduce.cpp
+++ benchmarks/bench_reduce.cpp
@@ -3,23 +3,21 @@
 
 #include "../include/parallel_for.h"
 
-static constexpr size_t MIN_SIZE = 1 << 10;
-static constexpr size_t MAX_SIZE = 1 << 26;
+static constexpr size_t MAX_SIZE = 1 << 24;
 static constexpr size_t BLOCK_SIZE = 1 << 10;
 
 static void BM_ReduceBench(benchmark::State &state) {
   InitParallel(GetNumThreads());
-  size_t size = state.range(0);
-  auto blocks = (size + BLOCK_SIZE - 1) / BLOCK_SIZE;
+  auto blocks = (MAX_SIZE + BLOCK_SIZE - 1) / BLOCK_SIZE;
   // allocate data and result once, reuse it for all iterations
-  auto data = SPMV::GenVector<double>(size);
+  auto data = SPMV::GenVector<double>(MAX_SIZE);
   std::vector<double> result(blocks);
 
   for (auto _ : state) {
     ParallelFor(0, blocks, [&](size_t i) {
       double sum = 0;
       auto start = i * BLOCK_SIZE;
-      auto end = std::min(start, size - BLOCK_SIZE) + BLOCK_SIZE;
+      auto end = std::min(start, MAX_SIZE - BLOCK_SIZE) + BLOCK_SIZE;
       for (size_t j = start; j < end; ++j) {
         sum += data[j];
       }
@@ -31,7 +29,6 @@ static void BM_ReduceBench(benchmark::State &state) {
 BENCHMARK(BM_ReduceBench)
     ->Name("Reduce_" + GetParallelMode())
     ->UseRealTime()
-    ->Unit(benchmark::kMicrosecond)
-    ->Range(MIN_SIZE, MAX_SIZE);
+    ->Unit(benchmark::kMicrosecond);
 
 BENCHMARK_MAIN();
