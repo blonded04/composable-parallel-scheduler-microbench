8b54ede1456b7126284bec84c72a9589eacdd948
Valery Matskevich <39431974+blonded04@users.noreply.github.com>
2024-10-09 19:17:16 +0300

    Update another-run.sh

--- another-run.sh
+++ another-run.sh
@@ -4,24 +4,28 @@ set -e
 conda init
 conda activate benchmarks
 
+rm -rf cmake-build-release
 rm -rf raw_results
 rm -rf bench_results
 OMP_NUM_THREADS=$1 numactl --cpunodebind 0 make bench
 cp -r raw_results raw_results_base
 cp -r bench_results bench_results_base
 
+rm -rf cmake-build-release
 rm -rf raw_results
 rm -rf bench_results
 OMP_NUM_THREADS=$1 numactl --cpunodebind 0 make bench_tf
 cp -r raw_results raw_results_tf
 cp -r bench_results bench_results_tf
 
+rm -rf cmake-build-release
 rm -rf raw_results
 rm -rf bench_results
 OMP_NUM_THREADS=$1 numactl --cpunodebind 0 make bench_lb4omp
 cp -r raw_results raw_results_lb4omp
 cp -r bench_results bench_results_lb4omp
 
+rm -rf cmake-build-release
 rm -rf raw_results
 rm -rf microresults
 mkdir microresults
