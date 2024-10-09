8abe7aed03735104ff12e90c001238d21824ff84
Valery Matskevich <39431974+blonded04@users.noreply.github.com>
2024-10-09 18:58:55 +0300

    Update another-run.sh

--- another-run.sh
+++ another-run.sh
@@ -5,21 +5,18 @@ conda activate benchmarks
 rm -rf raw_results
 rm -rf bench_results
 OMP_NUM_THREADS=$1 numactl --cpunodebind 0 make bench
-python3 benchplot.py
 cp -r raw_results raw_results_base
 cp -r bench_results bench_results_base
 
 rm -rf raw_results
 rm -rf bench_results
 OMP_NUM_THREADS=$1 numactl --cpunodebind 0 make bench_tf
-python3 benchplot.py
 cp -r raw_results raw_results_tf
 cp -r bench_results bench_results_tf
 
 rm -rf raw_results
 rm -rf bench_results
 OMP_NUM_THREADS=$1 numactl --cpunodebind 0 make bench_lb4omp
-python3 benchplot.py
 cp -r raw_results raw_results_lb4omp
 cp -r bench_results bench_results_lb4omp
 
