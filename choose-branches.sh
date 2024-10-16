512a6291c9f246624fa0ea51a78ef976170b188f
Valery Matskevich <generalretcher@gmail.com>
2024-10-16 15:32:40 +0300

    added fetchcontent as submodules
    Signed-off-by: Valery Matskevich <generalretcher@gmail.com>

---
+++ choose-branches.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -x
+
+set -e
+
+git submodule update --init
+
+cd onetbb
+git checkout v2021.13.0
+cd ../taskflow
+git checkout v3.8.0
+cd ../googletest
+git checkout release-1.12.0
+cd ../
+
