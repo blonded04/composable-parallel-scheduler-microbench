be4d328a7903aec7ab64637c808595b261c62dd3
Valery Matskevich <generalretcher@gmail.com>
2024-12-17 04:35:47 +0300

    Dockerfile FINALLY is ok
    Signed-off-by: Valery Matskevich <generalretcher@gmail.com>

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM ubuntu:22.04
 ENV DEBIAN_FRONTEND=noninteractive
 RUN apt-get update && \
-    apt-get install -y numactl git cmake software-properties-common build-essential libtool autoconf unzip wget g++ python3 python3-pip libomp-dev libhwloc-dev libjemalloc-dev patchelf elfutils pkg-config && \
+    apt-get install -y numactl git cmake software-properties-common build-essential linux-generic llvm libtool autoconf unzip wget clang g++ python3 python3-pip libomp-dev libhwloc-dev libjemalloc-dev patchelf elfutils pkg-config libc++-dev libc++abi-dev && \
     pip3 install matplotlib numpy seaborn pandas && \
     apt-get clean && \
     rm -rf /var/lib/apt/lists/*
--- run-me.sh
+++ run-me.sh
@@ -1,10 +1,16 @@
-#!/usr/bin/bash +x
+#!/usr/bin/bash
 
 set -e
+
+if [ "$#" -ne 1 ]; then
+    echo "Error: Exactly one argument is required."
+    exit 1
+fi
 
 rm -rf cmake-build-release
 rm -rf raw_results
 rm -rf bench_results
+cp cmakelists-variations/base.txt CMakeLists.txt
 OMP_NUM_THREADS=$1 numactl --cpunodebind 0 make bench
 cp -r raw_results raw_results_base
 cp -r bench_results bench_results_base
@@ -12,6 +18,7 @@ cp -r bench_results bench_results_base
 rm -rf cmake-build-release
 rm -rf raw_results
 rm -rf bench_results
+cp cmakelists-variations/taskflow.txt CMakeLists.txt
 OMP_NUM_THREADS=$1 numactl --cpunodebind 0 make bench_tf
 cp -r raw_results raw_results_tf
 cp -r bench_results bench_results_tf
@@ -19,6 +26,7 @@ cp -r bench_results bench_results_tf
 rm -rf cmake-build-release
 rm -rf raw_results
 rm -rf bench_results
+cp cmakelists-variations/lb4omp.txt CMakeLists.txt
 OMP_NUM_THREADS=$1 numactl --cpunodebind 0 make bench_lb4omp
 cp -r raw_results raw_results_lb4omp
 cp -r bench_results bench_results_lb4omp
